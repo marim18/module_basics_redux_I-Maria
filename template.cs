00c39ce530564d96522bd1484916089d2632d8ec
Christian Simonsen <crsdev@gmail.com>
2023-12-04 23:02:44 +0100

    added first draft of tasks, rubrics and submission gudlines

---
+++ template.cs
@@ -0,0 +1,38 @@
+// Your program should start at this line.
+/*
+Example:
+Move();
+Move();
+Move();
+Turn();
+Move();
+Move();
+Turn();
+Turn();
+Turn();
+Move();
+*/
+
+
+
+#region Basic functions
+// These functions are just her to make your intelisense work. 
+// They only have a conceptual function.
+
+void Move()
+{
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+void Turn()
+{
+    // Turns the car 90 deg clockwise.
+}
+
+bool Peek()
+{
+    // Returns true if the next cell is open, otherwise false.
+    return true // Just a placeholder value. 
+}
+
+#endregion
